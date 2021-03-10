e9f22f79a2ed3f5dd633dc320f321b9605c602ca
Reueltyler Moonsamy <19012817@vcconnect.co.za>
2021-03-10 09:42:59 +0200

    ye

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
